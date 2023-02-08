610b9463a15e01511aa4ab28a2bfdc32d8f64f8e
khalil ben romdhane <kh1925@outlook.com>
2023-02-08 01:39:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {Container, Navbar, Form, Button} from 'react-bootstrap';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="https://react-bootstrap.netlify.app/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default App;
